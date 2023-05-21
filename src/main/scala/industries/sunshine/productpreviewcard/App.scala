7ec8379dd1928ca6bcf1f6f51dd5d163bd0f9d2a
efim <efim.nefedov@nordigy.ru>
2023-05-21 23:43:03 +0400

    feat: making component take data as argument

--- src/main/scala/industries/sunshine/productpreviewcard/App.scala
+++ src/main/scala/industries/sunshine/productpreviewcard/App.scala
@@ -18,36 +18,47 @@ object Main {
   def appElement(): Element =
     div(
       className := "flex flex-col justify-center items-center w-screen h-screen bg-cream",
-      renderProductPreviewCard(),
+      renderProductPreviewCard(hardcodedProduct),
       renderAttribution()
     )
 
-  def renderProductPreviewCard() = {
+  def renderProductPreviewCard(product: ProductDescription) = {
     div(
       className := "bg-white rounded-lg w-[350px] h-[600px]",
       img(
-        src := "/images/image-product-mobile.jpg",
+        src := product.mobileImg,
         role := "img",
         className := "rounded-t-lg"
       ),
       div(
         className := "flex flex-col p-7",
-        p("Perfume", className := "text-sm text-gray-500 uppercase tracking-[.3rem]"),
         p(
-          "Gabrielle Essence Eau De Parfum",
+          product.category,
+          className := "text-sm text-gray-500 uppercase tracking-[.3rem]"
+        ),
+        p(
+          product.title,
           className := "py-3 font-serif text-4xl font-bold"
         ),
         p(
-          "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
+          product.description,
           className := "text-base text-gray-500"
         ),
         div(
-          className := "grid grid-cols-2 items-center pt-6 pb-5", 
-          p("$149.99", className := "font-serif text-4xl text-dark-cyan"),
-          p("$169.99", className := "text-sm text-gray-500 line-through")
+          className := "grid grid-cols-2 items-center pt-6 pb-5",
+          p(product.price, className := "font-serif text-4xl text-dark-cyan"),
+          product.oldPrice match {
+            case Some(crossedPrice) =>
+              p(crossedPrice, className := "text-sm text-gray-500 line-through")
+            case None => emptyNode
+          }
         ),
         button(
-          img(src := "/images/icon-cart.svg", role := "img", className := "px-3"),
+          img(
+            src := "/images/icon-cart.svg",
+            role := "img",
+            className := "px-3"
+          ),
           "Add to Cart",
           className := "w-full rounded-xl h-[3.25rem] bg-dark-cyan",
           className := "text-sm font-bold text-white",
@@ -71,4 +82,23 @@ object Main {
       a(href := "#", "Your Name Here")
     )
   }
+
+  final case class ProductDescription(
+      mobileImg: String,
+      desktopImg: String,
+      category: String,
+      title: String,
+      description: String,
+      price: String,
+      oldPrice: Option[String]
+  )
+  val hardcodedProduct = ProductDescription(
+    "/images/image-product-mobile.jpg",
+    "/images/image-product-desktop.jpg",
+    "Perfume",
+    "Gabrielle Essence Eau De Parfum",
+    "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
+    "$169.99",
+    Some("$199.99")
+  )
 }
